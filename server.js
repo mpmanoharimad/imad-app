4acf831d1fc2bc944b4a7affcaa8b683df9c3384
mpmanoharimad <30865581+mpmanoharimad@users.noreply.github.com>
2017-09-20 11:04:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ password:'db-mpmanohar-60798'
 var pool = new Pool(config);
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/test-db', function (req, res) {
