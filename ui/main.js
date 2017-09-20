df610de2a6bd16ae1eaf6bdb5c1f9b8ab6cb766a
mpmanoharimad <30865581+mpmanoharimad@users.noreply.github.com>
2017-09-20 11:39:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,19 @@ var span= document.getElementById('count');
 var counter=0;
 
 button.onclick = function() {
-    ++counter;
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+      if (request.readyState)  === XMLHttpRequest.DONE)
+      {
+          if ( request.status===200)
+          {
+              var counter=request.responseText;
+              span.innerHTML = counter.toString();
+          }
+      }
+    };
+    
+    request.open('GET', 'http://mpmanohar.imad.hasura-app.io/counter', true);
+    request.send();
 };
