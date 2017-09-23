e22cdd614049e8c79004a69c02be491b135fe0ff
mpmanoharimad <30865581+mpmanoharimad@users.noreply.github.com>
2017-09-23 08:52:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ function hash(input, salt) {
 app.get('/hash/:input', function (req, res) {
   var hashedString=hash(req.params.input, 'mysalt');
   var dbString="insert into user values('" + req.params.input + ",'" + hashedString + "');";
+  console.log(dbString);
   pool.query(dbString, function (err,result){
       if (err){
           res.status(500).send(err.toString());
